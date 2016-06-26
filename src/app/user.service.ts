47afb8e8c2a108096f8df8447c1e96c594def269
Mandal <172336@CTS.COM>
2016-06-26 18:34:03 -0400

    updated

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class UserService {
 
-  private usersUrl = '/api/users';  // URL to web api
+  private usersUrl = '/gateway/api/users';  // URL to web api
   constructor(private http: Http) { }
 
   getUsers(): Promise<User[]> {
